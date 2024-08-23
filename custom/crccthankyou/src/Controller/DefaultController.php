75e253875804698f2d3714d2b8309a84aacdfd2a
carlosantana07 <carlosantana07@hotmail.fr>
2024-08-23 15:02:48 -0400

    testing

--- custom/crccthankyou/src/Controller/DefaultController.php
+++ custom/crccthankyou/src/Controller/DefaultController.php
@@ -31,8 +31,10 @@ class DefaultController extends ControllerBase {
 
     return [
       '#type' => 'markup',
-      '#markup' => $this->t('')
-      . $translation,
+      '#markup' => $this->t('<h2>Thank you</h2>
+      <p>The Commission has received your online complaint. It will be reviewed to make certain that it meets the requirements of a public complaint under the <em>Royal Canadian Mounted Police Act</em>.</p>
+        <p>In cases where additional information is needed, an intake officer will contact you using the details you provided.</p>
+        <p>Your reference number is <strong>'.$_GET['result'].'</strong></p>'),
     ];
   }
 }
