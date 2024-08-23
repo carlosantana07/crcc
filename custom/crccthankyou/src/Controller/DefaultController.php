39248ce93df18e05cf00bbcf9b9be6a202053149
carlosantana07 <carlosantana07@hotmail.fr>
2024-08-23 15:13:36 -0400

    revert back the thank you page

--- custom/crccthankyou/src/Controller/DefaultController.php
+++ custom/crccthankyou/src/Controller/DefaultController.php
@@ -17,12 +17,21 @@ class DefaultController extends ControllerBase {
    */
   public function crccthankyou() {
 
+    $translation = '<h2>Thank you</h2>
+    <p>The Commission has received your online complaint. It will be reviewed to make certain that it meets the requirements of a public complaint under the <em>Royal Canadian Mounted Police Act</em>.</p>
+      <p>In cases where additional information is needed, an intake officer will contact you using the details you provided.</p>
+      <p>Your reference number is <strong>'.$_GET['result'].'</strong></p> ';
+
+      if(strcmp( $_SERVER['REQUEST_URI'],"/fr/thank-you")==0) {
+      $translation = '<h2>Merci</h2>
+    <p>La Commission a reçu votre plainte déposée en ligne. Nous l&#39;examinerons afin de nous assurer qu&#39;elle satisfait aux exigences de la <em>Loi sur la Gendarmerie royale du Canada</em>.</p>
+      <p>Si des renseignements supplémentaires, un agent des plaintes communiquera avec vous et se servira des détails que vous nous avez fournis.</p>
+      <p>Voici votre numéro de référence<strong>'.$_GET['result'].'</strong></p> ';
+    }
+
     return [
       '#type' => 'markup',
-      '#markup' => $this->t('<h2>Thank you</h2>')
-      . '<p>The Commission has received your online complaint. It will be reviewed to make certain that it meets the requirements of a public complaint under the <em>Royal Canadian Mounted Police Act</em>.</p>'
-      .'<p>In cases where additional information is needed, an intake officer will contact you using the details you provided.</p>'
-      .'<p>Your reference number is <strong>'.$_GET['result'].'</strong></p> ',
+      '#markup' => $this->t($translation),
     ];
   }
 
